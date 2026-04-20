dffc1c1e1fd16cc7093ca86ce3e4f89d1af1b2af
Gdev-z <gabrielzanon.dev@gmail.com>
2026-04-20 13:10:09 -0300

    second commit

--- GoodHamburger.Domain/obj/Debug/net8.0/GoodHamburger.Domain.AssemblyInfo.cs
+++ GoodHamburger.Domain/obj/Debug/net8.0/GoodHamburger.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoodHamburger.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99ebf65e364867180e051e7743a0b36a27feee50")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoodHamburger.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoodHamburger.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GoodHamburger.Infrastructure/obj/Debug/net8.0/GoodHamburger.Infrastructure.AssemblyInfo.cs
+++ GoodHamburger.Infrastructure/obj/Debug/net8.0/GoodHamburger.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoodHamburger.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99ebf65e364867180e051e7743a0b36a27feee50")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoodHamburger.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoodHamburger.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
